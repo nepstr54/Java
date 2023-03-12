634e4702edaa696f3f614a6423d410259fcce165
Alex Gryanko <116000789+nepstr54@users.noreply.github.com>
2023-03-13 00:22:58 +0300

    Task2

---
+++ task2.java
@@ -0,0 +1,25 @@
+// Дана последовательность целых чисел, оканчивающаяся нулем. 
+// Найти сумму положительных чисел, после которых следует отрицательное число.
+
+import java.util.Scanner;
+
+public class task2 {
+    public static void main(String[] args) {
+        int a, b = 1;
+        int sum = 0;
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Введите числа:");
+        a = scanner.nextInt();
+        
+        while (b != 0) {
+            b = scanner.nextInt();
+            if (a > 0 & b < 0) {
+                sum += a;
+            }
+            a = b;
+        }
+        System.out.println("Сумма = " + sum);
+        scanner.close();
+    }
+}
